9cf0da0f61e999cfd4ca1f0bdbc4c053af18b7d3
gittu-mf <1810247@iub.edu.bd>
2023-12-05 00:50:35 +0600

    z

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 function searchMeals() {
-    // Get the user input
+    
     const userInput = document.getElementById('searchInput').value;
 
-    // API URL for meal lookup
+    
     const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`;
 
-    // Fetch data from the API
+    
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => displayResults(data))
@@ -15,20 +15,20 @@ function searchMeals() {
 function displayResults(data) {
     const resultContainer = document.getElementById('result-container');
 
-    // Clear previous results
+    
     resultContainer.innerHTML = '';
 
-    // Check if there are any results
+    
     if (data.meals === null) {
         resultContainer.innerHTML = '<p>No results found.</p>';
         return;
     }
 
-    // Display the first 5 results
+    
     for (let i = 0; i < 5; i++) {
         const meal = data.meals[i];
 
-        // Create a card for each meal
+        
         const mealCard = document.createElement('div');
         mealCard.innerHTML = `
             <h3>${meal.strMeal}</h3>
