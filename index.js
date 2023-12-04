9e0df3f7b4fa3961709dcf6cc7812b14b190d26e
gittu-mf <1810247@iub.edu.bd>
2023-12-05 00:01:46 +0600

    js1

--- index.js
+++ index.js
@@ -1,5 +1,22 @@
+function searchMeals() {
+   
+    const userInput = document.getElementById('searchInput').value;
 
+    const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`;
 
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => displayResults(data))
+        .catch(error => console.error('Error fetching data:', error));
+}
+function displayResults(data) {
+    const resultContainer = document.getElementById('result-container');
 
     
-   
+    resultContainer.innerHTML = '';
+
+  
+    if (data.meals === null) {
+        resultContainer.innerHTML = '<p>No results found.</p>';
+        return;
+    }
