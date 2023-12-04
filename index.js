85f83dddad0b859e442440036d199767d4012291
gittu-mf <1810247@iub.edu.bd>
2023-12-05 00:11:12 +0600

    final chang2

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 function searchMeals() {
-   
     const userInput = document.getElementById('searchInput').value;
+
 
     const apiUrl = `https://www.themealdb.com/api/json/v1/1/search.php?s=${userInput}`;
 
+  
     fetch(apiUrl)
         .then(response => response.json())
         .then(data => displayResults(data))
         .catch(error => console.error('Error fetching data:', error));
 }
+
 function displayResults(data) {
     const resultContainer = document.getElementById('result-container');
 
-    
     resultContainer.innerHTML = '';
 
-  
+ 
     if (data.meals === null) {
         resultContainer.innerHTML = '<p>No results found.</p>';
         return;
     }
+
+    for (let i = 0; i < 5; i++) {
+        const meal = data.meals[i];
+
+     
+        const mealCard = document.createElement('div');
+        mealCard.innerHTML = `
+            <h3>${meal.strMeal}</h3>
+            <img src="${meal.strMealThumb}" alt="${meal.strMeal}" style="width: 200px; height: 150px;">
+            <p>${meal.strInstructions}</p>
+        `;
+
+        resultContainer.appendChild(mealCard);
+    }
+}
